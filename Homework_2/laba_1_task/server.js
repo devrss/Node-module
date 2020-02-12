7b08d047655de2866ef08402cd09cd0c2a02ea7a
418-san <fen1cs95@gmail.com>
2020-02-13 01:09:38 +0300

    Homework_2 (lab_2)

---
+++ Homework_2/laba_1_task/server.js
@@ -0,0 +1,20 @@
+let http = require('http');
+let fs = require('fs');
+
+http.createServer((request, response) => {
+    let pathname = 'site/index.html';
+    console.log("Request: " + request.url);
+    fs.readFile(pathname, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Could not find or open file for reading\n');
+            response.statusCode = 404;
+            response.end();
+        } else {
+            console.log(`The file ${pathname}is read and sent to the client\n`);
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        }
+    });
+}).listen(8080, () => {
+    console.log("HTTP server works in 8080 port!\n");
+});
